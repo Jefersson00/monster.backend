ae9c48ac2b5f4968571df2b186c82a28d47da54b
MarcoBraia <marco.braia.mb@gmail.com>
2020-09-22 12:45:04 +0200

    modifiche pom e controller

--- src/main/java/com/monster/backend/controller/AziendaController.java
+++ src/main/java/com/monster/backend/controller/AziendaController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import com.monster.persistence.entity.Azienda;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 @RequestMapping("/azienda")
 public class AziendaController {
 
